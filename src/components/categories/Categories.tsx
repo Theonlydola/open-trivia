9357a92e0684f4e959c9184cc92eab8736503778
theonlydola <mohamed.adel@trella.app>
2024-05-23 17:28:55 +0300

    feat: add categories page

---
+++ src/components/categories/Categories.tsx
@@ -0,0 +1,124 @@
+import { MouseEvent, useEffect, useState } from "react";
+import styled from "styled-components";
+import { useScoreContext, useSessionContext } from "../../contexts";
+import { useFetchCategories } from "../../queries";
+import { useNavigate } from "react-router-dom";
+
+export function Categories() {
+  const navigate = useNavigate();
+  const { sessionToken } = useSessionContext();
+  const categories = useFetchCategories({ enabled: !!sessionToken });
+  const { playedCategories, onScoreChange } = useScoreContext();
+  const [currentCategory, setCurrentCategory] = useState<number>(-1);
+
+  function onClick(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    setCurrentCategory(+event.currentTarget.name);
+  }
+
+  function onSubmit(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    onScoreChange?.("playedCategories", [...playedCategories, currentCategory]);
+    navigate("/quiz");
+  }
+
+  useEffect(() => {
+    return () => {
+      setCurrentCategory(-1);
+    };
+  }, []);
+
+  return (
+    <Container>
+      <h1>Questions Categories</h1>
+      <CategoriesGrid>
+        {categories.data?.trivia_categories.map(({ id, name }) => (
+          <CategoryCell
+            key={id}
+            name={String(id)}
+            onClick={onClick}
+            selected={currentCategory === id}
+            disabled={playedCategories?.includes(id)}
+          >
+            {name}
+          </CategoryCell>
+        ))}
+      </CategoriesGrid>
+      <Button onClick={onSubmit} disabled={currentCategory === -1}>
+        START
+      </Button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem;
+  gap: 1rem;
+  @media (max-width: 768px) {
+    padding: none;
+    h1 {
+      font-size: x-large;
+    }
+  }
+`;
+
+const CategoriesGrid = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(4, 1fr);
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+const CategoryCell = styled.button<{ selected?: boolean }>`
+  padding: 2rem;
+  background-color: ${(props) =>
+    props.selected ? props.theme.base200 : props.theme.base100};
+  color: black;
+  border-radius: 8px;
+  text-align: center;
+  text-overflow: ellipsis;
+  cursor: pointer;
+  &:disabled {
+    color: gray;
+    transform: none;
+    cursor: auto;
+    &:hover {
+      background-color: ${(props) => props.theme.base100};
+    }
+  }
+  &:hover {
+    background-color: ${(props) =>
+      props.selected ? props.theme.base200 : props.theme.base};
+  }
+  @media (max-width: 768px) {
+    padding: 1rem;
+  }
+`;
+
+const Button = styled.button`
+  border-radius: 8px;
+  border: 1px solid;
+  outline: none;
+  padding: 2rem;
+  cursor: pointer;
+  margin-top: 1rem;
+  background-color: ${(props) => props.theme.base100};
+  color: black;
+  &:hover {
+    background-color: ${(props) => props.theme.base};
+  }
+  &:disabled {
+    color: gray;
+    transform: none;
+    cursor: auto;
+    &:hover {
+      background-color: ${(props) => props.theme.base100};
+    }
+  }
+`;
